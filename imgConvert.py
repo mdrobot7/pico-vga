fb486ecdf24098b8a2f7a5936c77a38edf93e056
mdrobot7 <55768773+mdrobot7@users.noreply.github.com>
2022-09-11 13:19:56 -0500

    notes, image converter pseudocode

---
+++ imgConvert.py
@@ -0,0 +1,13 @@
+# import opencv
+
+#show image to be converted
+#ask for a new size
+#resize image, show it, ask if it's good
+
+#if the new image size is good:
+#resize the original image
+#make an array with the same dims, but only 1 index deep
+#for loop through original image
+# - do bitwise operations, conv back to decimal, save into new array
+
+#dump new array out to file in C const uint8_t array format

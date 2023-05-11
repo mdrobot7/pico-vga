beca64676463cf3af7cf641f2422af27a2d6ca24
mdrobot7 <55768773+mdrobot7@users.noreply.github.com>
2023-05-11 14:39:50 -0500

    Started audio
    After some research, decent quality WAV audio is actually incredibly simple -- IF you have a good source clock (fast enough and a multiple of your sample rate). I don't, so we'll see how it goes

--- lib/audio.c
+++ lib/audio.c
@@ -1,49 +1,103 @@
 #include "lib-internal.h"
+#include "hardware/pwm.h"
 
-/*
-How audio is going to work:
+#define MAX_SAMPLE_FREQ_HZ 44100
+#define MAX_SUPER_SAMPLE_RATE 3
 
-songs are going to be stored with a header and an array of Note_t's, each note holding
-*just* a frequency.
-
-when you want to play a song/sound, you run a function that sets up 4 dma channels:
-- L data -- copies data from the array to the PWM frequency register
-- L control -- resets the data channel so the sound loops (optional)
-- R data
-- R control
-
-Then, the function sets the DMA TREQ to a transfer pacing timer, with the timer frequency set to
-the BPM of the song / the smallest note (16th note, say, so div by 16).
-
-The array of Note_t's holds the song as if it was all 16th notes (for example), with imaginary
-slurs and ties to make notes longer. Then you can hand all of the transferring over to the DMA
-
-
-Alt solution (saves a bunch of RAM, but requries the CPU for stuff):
-Play a note, set a timer for the duration of the note that fires an interrupt when it expires. When
-the interrupt fires, reconfigure PWM, increment your position in the Note list, then
-reset the timer for the new note duration.
-
-Alt solution 2:
-Same as alt solution 1, but use PWM as your timer instead of the standard timer blocks.
-
-*/
-
-typedef struct __packed {
-    uint16_t freq;
-} Note_t;
+//TODO: scale _set_level for the different top values, figure out how the conversion script works
 
 typedef struct {
-    uint8_t defaultDuration;
-    uint8_t defaultOctave;
-    uint8_t defaultTempo; //BPM
-    Note_t * notes;
-} Sound_t;
+    uint32_t bitrate;
+    uint16_t sample_freq;
+    uint16_t super_sample_rate;
+    uint32_t len;
+    uint32_t num_channels;
+    uint8_t vol; //relative volume, for mixing. 0 (quietest) - 7 (loudest)
+    bool wrap;
+    uint16_t * data;
+    uint32_t data_i;
+} sound_t;
+
+#define SOUND_QUEUE_LEN 8
+static sound_t * sound_queue_l[SOUND_QUEUE_LEN];
+static sound_t * sound_queue_r[SOUND_QUEUE_LEN];
+
+static uint8_t volume = 1;
+
+irq_handler_t audio_irq_handler() {
+    uint32_t l = 0;
+    uint32_t r = 0;
+
+    uint8_t num_l = 0;
+    uint8_t num_r = 0;
+
+    /*
+    Takes all of the data for a particular channel and performs a weighted average. Also handles overflow.
+    */
+    for(int i = 0; i < SOUND_QUEUE_LEN; i++) {
+        if(sound_queue_l[i] != NULL) {
+            l += sound_queue_l[i]->data[sound_queue_l[i]->data_i/sound_queue_l[i]->super_sample_rate] << sound_queue_l[i]->vol;
+            sound_queue_l[i]->data_i++;
+            if(sound_queue_l[i]->data_i >= sound_queue_l[i]->len * sound_queue_l[i]->super_sample_rate) {
+                if(sound_queue_l[i]->wrap) sound_queue_l[i]->data_i = 0;
+                else sound_queue_l[i] = NULL;
+            }
+            num_l++;
+        }
+
+        if(sound_queue_r[i] != NULL) {
+            r += sound_queue_r[i]->data[sound_queue_r[i]->data_i/sound_queue_r[i]->super_sample_rate] << sound_queue_r[i]->vol;
+            sound_queue_r[i]->data_i++;
+            if(sound_queue_r[i]->data_i >= sound_queue_r[i]->len * sound_queue_r[i]->super_sample_rate) {
+                if(sound_queue_r[i]->wrap) sound_queue_r[i]->data_i = 0;
+                else sound_queue_r[i] = NULL;
+            }
+            num_r++;
+        }
+    }
+
+    if(num_l != 0) l /= num_l; //average the sound data for all of the currently playing sounds to mix them together
+    if(num_r != 0) r /= num_r;
+
+    l /= volume;
+    r /= volume;
+
+    pwm_set_gpio_level(AUDIO_L_PIN, l);
+    pwm_set_gpio_level(AUDIO_R_PIN, r);
+}
 
 int initAudio() {
+    if(clock_get_hz(clk_sys) < 120000000) return 1;
 
+    uint8_t slice_l = pwm_gpio_to_slice_num(AUDIO_L_PIN);
+    bool chan_l = pwm_gpio_to_channel(AUDIO_L_PIN);
+    uint8_t slice_r = pwm_gpio_to_slice_num(AUDIO_R_PIN);
+    bool chan_r = pwm_gpio_to_channel(AUDIO_R_PIN);
+
+    gpio_set_function(AUDIO_L_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(AUDIO_R_PIN, GPIO_FUNC_PWM);
+
+    pwm_set_wrap(slice_l, clock_get_hz(clk_sys)/(MAX_SUPER_SAMPLE_RATE * MAX_SAMPLE_FREQ_HZ));
+    pwm_set_wrap(slice_r, clock_get_hz(clk_sys)/(MAX_SUPER_SAMPLE_RATE * MAX_SAMPLE_FREQ_HZ));
+
+    pwm_set_chan_level(slice_l, chan_l, 0);
+    pwm_set_chan_level(slice_r, chan_r, 0);
+
+    pwm_set_enabled(slice_l, true);
+    pwm_set_enabled(slice_r, true);
+
+    pwm_set_irq_enabled(slice_l, true);
+    pwm_set_irq_enabled(slice_r, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, audio_irq_handler);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
 }
 
 int deInitAudio() {
-    return 0;
+    uint8_t slice_l = pwm_gpio_to_slice_num(AUDIO_L_PIN);
+    uint8_t slice_r = pwm_gpio_to_slice_num(AUDIO_R_PIN);
+
+    pwm_set_enabled(slice_l, false);
+    pwm_set_enabled(slice_r, false);
+
+    irq_set_enabled(PWM_IRQ_WRAP, false);
 }
